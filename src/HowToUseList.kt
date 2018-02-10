9a81987b9c5bc403de511f4b0fdae2144a8b057d
Epeng <ttt116@naver.com>
2018-02-10 14:07:32 +0900

    How to use list more added

--- src/HowToUseList.kt
+++ src/HowToUseList.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    var list: ArrayList<String> = ArrayList()
+    var arrayList: ArrayList<String> = ArrayList()
+    var array = listOf("1", "2", "3", "4")
+
+    arrayList.add("1")
+    arrayList.add("2")
+    arrayList.add("3")
+    arrayList.add("4")
+
+    array.forEach { print(it) }
+    println("")
+    arrayList.forEach { print(it) }
 }
